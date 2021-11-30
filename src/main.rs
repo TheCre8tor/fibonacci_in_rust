acc894a35e7183538ee9d6807d02fa80e072ae3c
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-11-30 10:29:23 +0100

    Completed the project

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::io;
+
 fn main() {
+    let mut user_input = String::new();
+    println!("\nEnter your Fibo Number: ");
+
+    io::stdin().read_line(&mut user_input).expect("Error Occured!");
+    let input_int = user_input.trim().parse::<u32>().unwrap();
+
     let mut n1: u32 = 0;
     let mut n2: u32 = 1;
     let mut next_term: u32;
@@ -6,7 +14,7 @@ fn main() {
     let mut index: u32 = 0;
 
     
-    while index < 21 {
+    while index < input_int {
         index = index + 1;
         println!("{}", n1);
 
